37ca01dd0e07797da8f611d0c60273cec654389a
JuampiGomez <juampi3217@gmail.com>
2024-06-03 16:11:38 -0300

    gravity

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const cxt = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const gravity = 0.5;
+
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
     };
     this.width = 50;
     this.height = 50;
@@ -18,7 +24,23 @@ class Player {
     cxt.fillStyle = "red";
     cxt.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
+      this.velocity.y += gravity;
+    } else this.velocity.y = 0;
+  }
 }
 
 const player = new Player();
-player.draw();
+
+function animate() {
+  cxt.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  window.requestAnimationFrame(animate);
+}
+
+animate();
